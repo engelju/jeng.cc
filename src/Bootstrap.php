60349d1a0b8cfa7457adbef29e910a3473360ede
Julie Engel <jue@metanet.ch>
2016-04-15 17:16:39 +0200

    install and setup flip/whoops error handler

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "now better hello world";
+namespace NoFw;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$env = 'devel';
+
+/* register error handler */
+$whoops = new \Whoops\Run;
+if ($env !== 'live') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e) {
+        echo 'Friendly error page and send email about error to developer';
+    });
+}
+$whoops->register();
+
+throw new \Exception('Hello World');
