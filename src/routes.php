4627b0c97b01951e0848b2fa4ce564356b0c6074
Julie Engel <jue@metanet.ch>
2016-04-21 14:20:36 +0200

    define root home page

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    ['GET', '/', ['NoFw\Controllers\HomepageController', 'show']],
     ['GET', '/hello-world', ['NoFw\Controllers\HomepageController', 'show']],
     ['GET', '/{slug}', ['NoFw\Controllers\PageController', 'show']],
 ];
